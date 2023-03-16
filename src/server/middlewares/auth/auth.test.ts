e1a54818ca65bd1503380d6188e6b3a5560065c1
DI3BOSS <diegofbermudez@gmail.com>
2023-03-16 23:34:35 +0100

    Test the auth middleware authorization header missing

---
+++ src/server/middlewares/auth/auth.test.ts
@@ -0,0 +1,32 @@
+import { type Request, type NextFunction, type Response } from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import auth from "./auth.js";
+import { type CustomAuthRequest } from "../../../types.js";
+import CustomError from "../../../CustomError/CustomError";
+import errors from "../../constants/errors.js";
+
+const response: Partial<Response> = {};
+const next: NextFunction = jest.fn();
+
+describe("Given the token auth middleware", () => {
+  describe("When it receives a request without an authorization header", () => {
+    test("Then it should call the next function with and error status code 401 and message 'Authorization header missing'", () => {
+      const request: Partial<CustomAuthRequest> = {
+        header: jest.fn().mockReturnValue(undefined),
+      };
+      const expectedStatus = errors.unauthorized.statusCode;
+      const expectedError = new CustomError(
+        errors.unauthorized.authHeaderMissing,
+        expectedStatus,
+        errors.unauthorized.authInvalidToken
+      );
+
+      jwt.verify = jest.fn().mockReturnValueOnce({});
+
+      auth(request as CustomAuthRequest, response as Response, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
