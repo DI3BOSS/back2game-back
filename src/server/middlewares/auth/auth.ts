18b15e810b5765ae82f1998cb03ff219dc7c860c
DI3BOSS <diegofbermudez@gmail.com>
2023-03-16 23:09:36 +0100

    Implement the token auth middleware

---
+++ src/server/middlewares/auth/auth.ts
@@ -0,0 +1,44 @@
+import { type Response, type NextFunction, Request } from "express";
+import jwt from "jsonwebtoken";
+import CustomError from "../../../CustomError/CustomError.js";
+import { type CustomAuthRequest } from "../../../types.js";
+import errors from "../../constants/errors.js";
+import { type CustomJwtPayload } from "../../controllers/types.js";
+
+const auth = (
+  request: CustomAuthRequest,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const authorizationRequestHeader = request.header("Authorization");
+
+    if (!authorizationRequestHeader) {
+      throw new Error(errors.unauthorized.authHeaderMissing);
+    }
+
+    if (!authorizationRequestHeader.startsWith("Bearer ")) {
+      throw new Error(errors.unauthorized.authBearerMissing);
+    }
+
+    const token = authorizationRequestHeader.replace(/^Bearer\s*/, "");
+
+    const { sub: postedBy } = jwt.verify(
+      token,
+      process.env.JWT_SECRET!
+    ) as CustomJwtPayload;
+
+    request.postedBy = postedBy;
+
+    next();
+  } catch (error: unknown) {
+    const tokenError = new CustomError(
+      (error as Error).message,
+      401,
+      errors.unauthorized.authInvalidToken
+    );
+    next(tokenError);
+  }
+};
+
+export default auth;
