708df6f22fddbfe3cf5758f387392e67f8cd4f50
DI3BOSS <diegofbermudez@gmail.com>
2023-03-17 15:10:09 +0100

    Implement deleteGame controller with auth control

--- src/server/controllers/gamesControllers/gamesControllers.ts
+++ src/server/controllers/gamesControllers/gamesControllers.ts
@@ -1,6 +1,7 @@
 import { type NextFunction, type Request, type Response } from "express";
 import CustomError from "../../../CustomError/CustomError.js";
 import Game from "../../../database/models/Game.js";
+import { type CustomAuthRequest, CustomRequest } from "../../../types.js";
 import errors from "../../constants/errors.js";
 import successes from "../../constants/successes.js";
 
@@ -21,6 +22,25 @@ export const getGames = async (
     }
 
     res.status(successes.ok.statusCode).json({ games });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteGame = async (
+  req: CustomAuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { idGame } = req.params;
+
+  try {
+    const game = await Game.findByIdAndDelete({
+      _id: idGame,
+      postedBy: req.postedBy,
+    }).exec();
+
+    res.status(successes.ok.statusCode).json({ game });
   } catch (error) {
     next(error);
   }
--- src/server/routers/gamesRouter.ts
+++ src/server/routers/gamesRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import endpoints from "../constants/endpoint.js";
+import endpoints from "../constants/endpoints.js";
 import { getGames } from "../controllers/gamesControllers/gamesControllers.js";
 
 const gamesRouter = Router();
